9d5b55254c0000789ae9a2c3998f5dfc71fa87c3
huangbo <1062361820@qq.com>
2020-12-20 16:17:58 +0800

    提交面试的最终版

--- src/components/LoginOrRegisterCard/LoginOrRegisterCard.tsx
+++ src/components/LoginOrRegisterCard/LoginOrRegisterCard.tsx
@@ -1,39 +1,3 @@
-// import React, { Component, Fragment } from 'react';
-// import style from './LoginOrRegisterCard.module.scss';
-// import avatalImg from './images/avatal.png';
-
-// type StateType = {
-//   [propName: string]: any;
-// };
-// type PropType = {
-//   errorPrompt?: string; // 错误提示
-//   [propName: string]: any;
-// };
-// interface LoginOrRegisterCard {
-//   state: StateType;
-//   props: PropType;
-// }
-// class LoginOrRegisterCard extends Component {
-//   render() {
-//     const { children, errorPrompt } = this.props
-//     return (
-//       <div className={style.LoginOrRegisterCard}>
-//         <div className={style.avatal}>
-//           <img src={avatalImg} alt="LoginOrRegisterCardAvatalImg"/>
-//         </div>
-//         {errorPrompt && <div className={style.error}>{errorPrompt}</div>}
-//         <Fragment>
-//           {children}
-//         </Fragment>
-//       </div>
-//     )
-//   }
-// }
-
-// export default LoginOrRegisterCard;
-
-
-
 import React, { Fragment } from 'react';
 import style from './LoginOrRegisterCard.module.scss';
 import avatalImg from './images/avatal.png';
